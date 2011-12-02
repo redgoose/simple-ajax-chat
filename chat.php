bf7642345c37596d78f8abd4f72adab3b83bc1a5
Tariq M <t@redgoose.ca>
2011-12-01 22:17:15 -0500

    fixed indenting

--- chat.php
+++ chat.php
@@ -5,48 +5,48 @@ class Chat {
     private $initial_lines = 5;
 	
     public function __construct() {
-		$this->db = new SQLiteDatabase('db/c.db');
-		//$this->setupDb();
-	    $this->truncateDb();
+        $this->db = new SQLiteDatabase('db/c.db');
+        //$this->setupDb();
+        $this->truncateDb();
     }
 
-	public function getLog($offset) {
+    public function getLog($offset) {
         $query = $this->db->query("select max(rowid) from chat");
         $maxid = $query->fetchSingle();		
-	    if (!$maxid) { $maxid = 0; }
-        
+        if (!$maxid) { $maxid = 0; }
+
         if (!$offset) {
             $offset = $maxid - $this->initial_lines;
-		}
+        }
 
-		$sql = "select rowid,n,m from chat where rowid > $offset";
-		$query = $this->db->unbufferedQuery($sql);
-		
+        $sql = "select rowid,n,m from chat where rowid > $offset";
+        $query = $this->db->unbufferedQuery($sql);
+
         $json = array("msgs"=>array(),"offset"=>$maxid);
-		
+
         while ($entry = $query->fetch(SQLITE_ASSOC)) {
-			$name = $entry['n'];
-			$msg = $entry['m'];
-    		array_push($json["msgs"],array("n"=>$name,"m"=>$msg));
-		}
+            $name = $entry['n'];
+            $msg = $entry['m'];
+            array_push($json["msgs"],array("n"=>$name,"m"=>$msg));
+        }
 
         return json_encode($json);
-	}
+    }
 
-	public function insertMsg($msg) {
-		$sql = "insert into chat values("
-		. "'" . sqlite_escape_string($msg['name']) . "',"
-		. "'" . sqlite_escape_string($msg['msg']) . "'"
-		. ")";
-		$this->db->queryExec($sql);
-	}
+    public function insertMsg($msg) {
+        $sql = "insert into chat values("
+        . "'" . sqlite_escape_string($msg['name']) . "',"
+        . "'" . sqlite_escape_string($msg['msg']) . "'"
+        . ")";
+        $this->db->queryExec($sql);
+    }
 
     private function setupDb() {
-		$this->db->queryExec("drop table chat");
-		$this->db->queryExec("create table chat (n varchar(1), m text)");	
-		//$this->db->queryExec("insert into chat values ('t','1')");
-	}
-    
+        $this->db->queryExec("drop table chat");
+        $this->db->queryExec("create table chat (n varchar(1), m text)");	
+        //$this->db->queryExec("insert into chat values ('t','1')");
+    }
+
     private function truncateDb() {
         $sql = "delete from chat where rowid not in (select rowid from chat order by rowid desc limit 100)";
         $this->db->queryExec($sql);
--- json.php
+++ json.php
@@ -1,16 +1,16 @@
 <?php
     require 'chat.php';
 
-	$chat = new Chat();
+    $chat = new Chat();
 
-	if ($_GET["a"] == "i") {
-		$chat->insertMsg(array("name"=>$_GET["n"],"msg"=>$_GET["m"]));	
-	}
+    if ($_GET["a"] == "i") {
+        $chat->insertMsg(array("name"=>$_GET["n"],"msg"=>$_GET["m"]));	
+    }
 
     $offset = null;
     if ($_GET["o"]) {
         $offset = $_GET["o"];
     }
-	$log = $chat->getLog($offset);
-	print $log;
+    $log = $chat->getLog($offset);
+    print $log;
 ?>
